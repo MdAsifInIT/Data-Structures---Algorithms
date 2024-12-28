d4755d9e5cbca9315b4a4fe96cc01e7eec3d2d45
MdAsifInIT <yb.fisa@gmail.com>
2024-12-29 00:23:21 +0530

    renamed:    99 - Acrhive/00 - BITS Pilani/Assignments/01_Assignment1.c -> 99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 1/01_Assignment1.c 	renamed:    99 - Acrhive/00 - BITS Pilani/Assignments/02_Assignment1.c -> 99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 2/02_Assignment1.c 	renamed:    99 - Acrhive/00 - BITS Pilani/Assignments/01_Assigments3.c -> 99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 3/03_Assigments3.c 	new file:   99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 4/01_Assignment4.c 	new file:   99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 4/02_Assignment4.c 	new file:   99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 4/03_Assignment4.c 	deleted:    99 - Acrhive/00 - BITS Pilani/Assignments/Rand/01_Assigment3.c

---
+++ 99 - Acrhive/00 - BITS Pilani/Assignments/Assignment 4/02_Assignment4.c
@@ -0,0 +1,37 @@
+// 2. Write a C program to compare two strings without using the strcmp function.
+
+#include <stdio.h>
+#include <string.h>
+
+int compareStrings(const char* str1, const char* str2) {
+    while (*str1 && *str2) {
+        if (*str1 != *str2)
+            return *str1 - *str2;
+        str1++;
+        str2++;
+    }
+    return *str1 - *str2; // Handles cases where strings are of different lengths
+}
+
+int main() {
+    char str1[100], str2[100];
+
+    printf("Enter the first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Enter the second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    // Remove newline characters from fgets input
+    str1[strcspn(str1, "\n")] = '\0';
+    str2[strcspn(str2, "\n")] = '\0';
+
+    int result = compareStrings(str1, str2);
+    if (result == 0)
+        printf("The strings are equal.\n");
+    else if (result < 0)
+        printf("The first string is lexicographically smaller.\n");
+    else
+        printf("The first string is lexicographically larger.\n");
+
+    return 0;
+}
